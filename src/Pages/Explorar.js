88d37c5f2f926cd5e1fb99182c9e66e6589db4b6
GregoryG8 <gregory.gonzalez@correounivalle.edu.co>
2023-06-23 22:32:48 -0500

    organizacion

---
+++ src/Pages/Explorar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { LeftMenu } from "../Components/LeftMenu"
+import { Header } from "../Components/HeaderBar"
+
+function Explorar() {
+  return (
+    <div className='explorar' id='background'>
+      <LeftMenu />
+      <Header />
+    </div>
+  )
+}
+
+export {Explorar}
