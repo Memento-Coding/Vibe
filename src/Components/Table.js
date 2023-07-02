88aa3040c706c48cdfe4ed6cf8560a64a317958e
andres5403 <didier.arce@correounivalle.edu.co>
2023-07-02 10:35:59 -0500

    perfil de usuario full

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,126 +1,102 @@
-import React from 'react'
-import '../Styles/Table.css'
+import React from 'react';
+import '../Styles/Table.css';
 import MaterialTable from 'material-table';
 import { ThemeProvider, createTheme } from '@mui/material';
-import Delete from '@material-ui/icons/Delete';
-import { Edit} from '@material-ui/icons';
-import { PlayArrow } from '@material-ui/icons';
-
-
+import { Delete, Edit, PlayArrow } from '@material-ui/icons';
 
 function Table() {
-  
   const customTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#0D2B41', // Cambia el color 
-        backgroundColor: "#0D2B41"
-      },
-    },
-    typography: {
-      fontFamily: 'Poppins', // Cambia el tipo de letra predeterminado
-      fontSize: 16, // Cambia el tamaño de fuente predeterminado
-      
-    },
+    // Configuración del tema personalizado
   });
 
   const columnas = [
     {
-        title:"Titulo",
-        field:"titulo"
+      title: 'Titulo',
+      field: 'titulo',
     },
     {
-        title:"Artista",
-        field:"artista"
+      title: 'Artista',
+      field: 'artista',
     },
     {
-        title:"Genero",
-        field:"genero"
+      title: 'Genero',
+      field: 'genero',
     },
-
+    {
+      title: 'Acciones',
+      render: (rowData) => (
+        <>
+          <PlayArrow
+            onClick={() => handleIconClick(rowData.titulo, 'Play')}
+            style={{ cursor: 'pointer' }}
+          />
+          <Edit
+            onClick={() => handleIconClick(rowData.titulo, 'Editar')}
+            style={{ cursor: 'pointer' }}
+          />
+          <Delete
+            onClick={() => handleIconClick(rowData.titulo, 'Eliminar')}
+            style={{ cursor: 'pointer' }}
+          />
+        </>
+      ),
+    },
   ];
 
   const data = [
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    {titulo:"mutter", artista:"Rammstein", genero:"Heavy metal"},
-    
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
+    { titulo: 'mutter', artista: 'Rammstein', genero: 'Heavy metal' },
   ];
 
   const tableOptions = {
+    paging: false,
+    search: false,
+    tableLayout: 'fixed',
     headerStyle: {
-      backgroundColor: '#F2F2F2',
+      backgroundColor: '#0D2B41',
+      color: '#ffffff',
+      fontFamily: 'Poppins',
+      fontSize: '20px',
     },
+    cellStyle: {
+      backgroundColor: '#101010',
+      color: '#ffffff',
+      fontFamily: 'Arial',
+      fontSize: '14px',
+    },
+    responsive: 'scroll',
+    padding: 'dense',
+    toolbar: false,
+    
   };
 
+  const handleIconClick = (titulo, accion) => {
+    alert(`${accion}: ${titulo}`);
+  };
 
   return (
-    <div className='Container-table'>
-        <div style={{ width: '100%' }}>
-           <ThemeProvider theme={customTheme}>
-                <MaterialTable
-                  columns={columnas}
-                  data={data}
-                  title={"Que canciones te gustan"}
-                  actions={[
-                    {
-                      icon:PlayArrow,
-                      tooltip: "Escuchar cancion",
-                      onClick: (event, rowData)=>{
-                        alert("Play")
-                      }
-                      
-                    },
-                    {
-                      icon:Edit,
-                      tooltip: "Editar cancion",
-                      onClick: (event, rowData)=>{
-                        alert("Editar")
-                      }
-                      
-                    },
-                    {
-                      icon:Delete,
-                      tooltip: "Eliminar cancion",
-                      onClick: (event, rowData)=>alert("Eliminar")
-                      
-                      
-                    },
-
-                  ]}
-                  components={{
-                    Toolbar: () => null, // Oculta la barra de herramientas predeterminada
-                  }}
-                  options={{
-                    paging: false,
-                    search:false,
-                    TablePagination: false,
-                    headerStyle: {
-                        backgroundColor: '#0D2B41',
-                        color:"white",
-                        fontFamily: 'Poppins', // Cambia el tipo de letra aquí
-                        fontSize: '20px', // Cambia el tamaño de la fuente aquí
-                      },
-                    cellStyle: {
-                        backgroundColor: '#0D2B41',
-                        color:"white",
-                        fontFamily: 'Arial', // Cambia el tipo de letra aquí
-                        fontSize: '14px', // Cambia el tamaño de la fuente aquí
-                  
-                      },
-                    
-                      
-              
-                    
-                    
-                  }}
-                />
-            </ThemeProvider>
-        </div>
-        
+    <div className="Container-table">
+      <div style={{ width: '100%' }}>
+        <ThemeProvider theme={customTheme}>
+          <MaterialTable
+            columns={columnas}
+            data={data}
+            title="Que canciones te gustan"
+            components={{
+              Toolbar: () => null, // Oculta la barra de herramientas predeterminada
+            }}
+            options={tableOptions}
+          />
+        </ThemeProvider>
+      </div>
     </div>
   );
 }
