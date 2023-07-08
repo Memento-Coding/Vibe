3d336d4fbb53370696b2f9c6b57f0f6ad90f365f
Juan Hoyos <juanhoyos1347@gmail.com>
2023-07-08 02:09:32 -0500

    login google

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBV3cPRudJZS56M5aMedjDfxTGMN4fIWuM",
-  authDomain: "vibe-1daa4.firebaseapp.com",
-  projectId: "vibe-1daa4",
-  storageBucket: "vibe-1daa4.appspot.com",
-  messagingSenderId: "539369077198",
-  appId: "1:539369077198:web:31f0ca62a11a96047d139f"
+  apiKey: "AIzaSyAAnJzObRNCxgPppnzmb1Tor2fnWA6oyBA",
+  authDomain: "vibe-1a1bb.firebaseapp.com",
+  projectId: "vibe-1a1bb",
+  storageBucket: "vibe-1a1bb.appspot.com",
+  messagingSenderId: "839547539415",
+  appId: "1:839547539415:web:d699c70ac28c447b2c8592"
 };
 
 // Initialize Firebase
